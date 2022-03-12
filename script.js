a6e0e10da66db9fd4d6729eaf4840e67f76ef0d9
DaSpaceCat <DaSpaceCat@users.noreply.github.com>
2022-03-12 16:38:55 -0700

    fix image 8 not loading because it's a gif

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var prevWinWidth;
 
 $(document).ready(function() {
   for (let i = 10; i > 0; i--) {
-    $("#blends").after('<image class="projectImage" src="https://raw.githubusercontent.com/DaSpaceCat/DaSpaceCat/main/assets/' + i + '.png">');
+    if (i == 8) {
+      $("#blends").after('<image class="projectImage" src="https://raw.githubusercontent.com/DaSpaceCat/DaSpaceCat/main/assets/' + i + '.gif">');
+    } else {
+      $("#blends").after('<image class="projectImage" src="https://raw.githubusercontent.com/DaSpaceCat/DaSpaceCat/main/assets/' + i + '.png">');
+    }
   }
 
   function update() {
