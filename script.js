b3db88850c267d2df4111eab333dae2e698f1148
DaSpaceCat <DaSpaceCat@users.noreply.github.com>
2022-03-12 21:34:45 -0700

    overhaul of desktop css, change images

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ var winWidth;
 var prevWinWidth;
 
 $(document).ready(function() {
-  for (let i = 10; i > 0; i--) {
+  // commented out because i'm no longer using this to init my images.
+  /*for (let i = 10; i > 0; i--) {
     if (i == 8) {
       $("#blends").after('<image class="projectImage" src="https://raw.githubusercontent.com/DaSpaceCat/DaSpaceCat/main/assets/' + i + '.gif">');
     } else {
       $("#blends").after('<image class="projectImage" src="https://raw.githubusercontent.com/DaSpaceCat/DaSpaceCat/main/assets/' + i + '.png">');
     }
-  }
+  }*/
 
   function update() {
     winWidth = $(window).width();
